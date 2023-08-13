0ac50bebbe8efd38bf20489bc2e5d359746a2fcd
samarthkalashetty <samarthkalashetti2612@gmail.com>
2023-08-13 15:33:14 +0530

    Welcome to UC8

--- Program.cs
+++ Program.cs
@@ -1,75 +1,64 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 
 class Contact
 {
     public string Name { get; set; }
     public string PhoneNumber { get; set; }
     public string Email { get; set; }
+    public string City { get; set; }
+    public string State { get; set; }
 }
 
 class Program
 {
     static void Main(string[] args)
     {
-        List<Contact> addressBook = new List<Contact>
+        List<Contact> addressBook1 = new List<Contact>
         {
-            new Contact { Name = "Alice", PhoneNumber = "123-456-7890", Email = "alice@example.com" },
-            new Contact { Name = "Bob", PhoneNumber = "987-654-3210", Email = "bob@example.com" }
+            new Contact { Name = "Samarth", PhoneNumber = "9960852612", Email = "Sam9960852612@gmail.com", City = "Solapur", State = "Maharashtra" },
+            new Contact { Name = "Bob", PhoneNumber = "987-654-3210", Email = "bob@example.com", City = "Los Angeles", State = "CA" }
         };
 
-        string filePath = "addressbook.txt";
-
-        // Write address book to file
-        WriteToFile(addressBook, filePath);
-
-        // Read address book from file
-        List<Contact> readAddressBook = ReadFromFile(filePath);
-
-        // Display the read contacts
-        foreach (Contact contact in readAddressBook)
+        List<Contact> addressBook2 = new List<Contact>
         {
-            Console.WriteLine($"Name: {contact.Name}, Phone: {contact.PhoneNumber}, Email: {contact.Email}");
+            new Contact { Name = "Charlie", PhoneNumber = "111-222-3333", Email = "charlie@example.com", City = "New York", State = "NY" },
+            new Contact { Name = "David", PhoneNumber = "555-666-7777", Email = "david@example.com", City = "Chicago", State = "IL" }
+        };
+
+        List<Contact> allContacts = new List<Contact>();
+        allContacts.AddRange(addressBook1);
+        allContacts.AddRange(addressBook2);
+
+        Console.Write("Enter the city to search: ");
+        string searchCity = Console.ReadLine();
+
+        Console.Write("Enter the state to search: ");
+        string searchState = Console.ReadLine();
+
+        List<Contact> searchResults = SearchContactsByCityOrState(allContacts, searchCity, searchState);
+
+        if (searchResults.Count > 0)
+        {
+            Console.WriteLine($"Search results in {searchCity}, {searchState}:");
+            foreach (Contact contact in searchResults)
+            {
+                Console.WriteLine($"Name: {contact.Name}, Phone: {contact.PhoneNumber}, Email: {contact.Email}");
+            }
+        }
+        else
+        {
+            Console.WriteLine("No results found.");
         }
     }
 
-    static void WriteToFile(List<Contact> contacts, string filePath)
+    static List<Contact> SearchContactsByCityOrState(List<Contact> contacts, string city, string state)
     {
-        using (StreamWriter writer = new StreamWriter(filePath))
-        {
-            foreach (Contact contact in contacts)
-            {
-                writer.WriteLine($"{contact.Name},{contact.PhoneNumber},{contact.Email}");
-            }
-        }
-
-        Console.WriteLine("Address book written to file.");
-    }
-
-    static List<Contact> ReadFromFile(string filePath)
-    {
-        List<Contact> addressBook = new List<Contact>();
-
-        using (StreamReader reader = new StreamReader(filePath))
-        {
-            string line;
-            while ((line = reader.ReadLine()) != null)
-            {
-                string[] parts = line.Split(',');
-                if (parts.Length == 3)
-                {
-                    addressBook.Add(new Contact
-                    {
-                        Name = parts[0],
-                        PhoneNumber = parts[1],
-                        Email = parts[2]
-                    });
-                }
-            }
-        }
-
-        Console.WriteLine("Address book read from file.");
-        return addressBook;
+        return contacts.Where(contact =>
+            contact.City.Equals(city, StringComparison.OrdinalIgnoreCase) ||
+            contact.State.Equals(state, StringComparison.OrdinalIgnoreCase))
+            .ToList();
     }
 }
