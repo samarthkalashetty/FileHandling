73f4014909dc34cf222c5566989ab79da24d779e
samarthkalashetty <samarthkalashetti2612@gmail.com>
2023-08-13 13:44:02 +0530

    Welcome to FileHandling

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace FileHandling
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to File Handling");
+        }
+    }
+}
