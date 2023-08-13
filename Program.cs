34523a23c2231f88e64f0d145396c9d5901aab1d
samarthkalashetty <samarthkalashetti2612@gmail.com>
2023-08-13 16:05:05 +0530

    Welcome to UC12

--- Program.cs
+++ Program.cs
@@ -8,15 +8,23 @@ class Contact : IComparable<Contact>
     public string Email { get; set; }
     public string City { get; set; }
     public string State { get; set; }
+    public string ZipCode { get; set; }
 
     public int CompareTo(Contact other)
     {
-        return string.Compare(Name, other.Name, StringComparison.OrdinalIgnoreCase);
+        // Compare by City, then by State, then by Zip
+        int cityComparison = string.Compare(City, other.City, StringComparison.OrdinalIgnoreCase);
+        if (cityComparison != 0) return cityComparison;
+
+        int stateComparison = string.Compare(State, other.State, StringComparison.OrdinalIgnoreCase);
+        if (stateComparison != 0) return stateComparison;
+
+        return string.Compare(ZipCode, other.ZipCode, StringComparison.OrdinalIgnoreCase);
     }
 
     public override string ToString()
     {
-        return $"Name: {Name}, Phone: {PhoneNumber}, Email: {Email}, City: {City}, State: {State}";
+        return $"Name: {Name}, Phone: {PhoneNumber}, Email: {Email}, City: {City}, State: {State}, Zip: {ZipCode}";
     }
 }
 
@@ -26,30 +34,49 @@ class Program
     {
         List<Contact> addressBook = new List<Contact>
         {
-            new Contact { Name = "Alice", PhoneNumber = "123-456-7890", Email = "alice@example.com", City = "New York", State = "NY" },
-            new Contact { Name = "Bob", PhoneNumber = "987-654-3210", Email = "bob@example.com", City = "Los Angeles", State = "CA" },
-            new Contact { Name = "Charlie", PhoneNumber = "111-222-3333", Email = "charlie@example.com", City = "New York", State = "NY" },
-            new Contact { Name = "David", PhoneNumber = "555-666-7777", Email = "david@example.com", City = "Chicago", State = "IL" }
+            new Contact { Name = "Alice", PhoneNumber = "123-456-7890", Email = "alice@example.com", City = "New York", State = "NY", ZipCode = "10001" },
+            new Contact { Name = "Bob", PhoneNumber = "987-654-3210", Email = "bob@example.com", City = "Los Angeles", State = "CA", ZipCode = "90001" },
+            new Contact { Name = "Charlie", PhoneNumber = "111-222-3333", Email = "charlie@example.com", City = "New York", State = "NY", ZipCode = "10002" },
+            new Contact { Name = "David", PhoneNumber = "555-666-7777", Email = "david@example.com", City = "Chicago", State = "IL", ZipCode = "60601" }
         };
 
         Console.WriteLine("Original Address Book:");
         PrintContacts(addressBook);
 
-        Console.WriteLine("\nEnter 'sort' to sort the address book by name:");
-        string userInput = Console.ReadLine();
+        Console.Write("\nEnter 'city', 'state', or 'zip' to sort the address book: ");
+        string sortBy = Console.ReadLine();
 
-        if (userInput.Equals("sort", StringComparison.OrdinalIgnoreCase))
+        List<Contact> sortedAddressBook = SortAddressBook(addressBook, sortBy);
+
+        if (sortedAddressBook != null)
         {
-            // Sort the address book by name
-            addressBook.Sort();
-
-            Console.WriteLine("\nSorted Address Book:");
-            PrintContacts(addressBook);
+            Console.WriteLine($"\nSorted Address Book by {sortBy}:");
+            PrintContacts(sortedAddressBook);
         }
         else
         {
-            Console.WriteLine("No sorting performed.");
+            Console.WriteLine("Invalid input. No sorting performed.");
         }
+    }
+
+    static List<Contact> SortAddressBook(List<Contact> contacts, string sortBy)
+    {
+        switch (sortBy.ToLower())
+        {
+            case "city":
+                contacts.Sort((contact1, contact2) => contact1.City.CompareTo(contact2.City));
+                break;
+            case "state":
+                contacts.Sort((contact1, contact2) => contact1.State.CompareTo(contact2.State));
+                break;
+            case "zip":
+                contacts.Sort((contact1, contact2) => contact1.ZipCode.CompareTo(contact2.ZipCode));
+                break;
+            default:
+                return null;
+        }
+
+        return contacts;
     }
 
     static void PrintContacts(List<Contact> contacts)
