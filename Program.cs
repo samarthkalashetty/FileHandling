96e151985dfe0c754746f8d81094d5599f0eea50
samarthkalashetty <samarthkalashetti2612@gmail.com>
2023-08-13 14:00:31 +0530

    Welcome to start

--- Program.cs
+++ Program.cs
@@ -1,10 +1,25 @@
-﻿namespace FileHandling
+﻿using System;
+using System.Collections.Generic;
+
+class AddressBookMain
 {
-    internal class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Welcome to File Handling");
-        }
+        Console.WriteLine("Welcome to Address Book Program in C#");
+
+        // Create a list to store addresses
+        List<Address> addressList = new List<Address>();
+
+        // Your code logic for the address book can go here
+
+        Console.WriteLine("Address Book program has ended.");
     }
+}
+
+class Address
+{
+    public string Name { get; set; }
+    public string PhoneNumber { get; set; }
+    public string Email { get; set; }
+    public string AddressInfo { get; set; }
 }
